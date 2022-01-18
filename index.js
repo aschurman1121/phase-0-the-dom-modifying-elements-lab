b88625c23d666ad6aedb4a91041604f860e4c86c
alexandra <alexandraschurman@gmail.com>
2022-01-17 18:36:15 -0800

    commit tree a82811a88a7b00801f07dca57981caa457023273

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // Write your code here!
-const main = document.querySelector("#main")
+// const element = document.createElement("div");
 
+const main = document.getElementById("main")
 main.remove()
 
-const newHeader = document.createElement('h1')
-newHeader.setAttribute("id", "victory")
+const newHeader = document.createElement("h1")
 
+newHeader.setAttribute("id", 'victory')
 
-newHeader.innerHTML = 'Ali is the champion'
-
-document.body.append(newHeader)
+newHeader.textContent = "Ali is the champion"
